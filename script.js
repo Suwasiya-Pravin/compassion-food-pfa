c9f26c64a89e53da2caf8cc7715aea2f1f61a24c
Pravin Suwasiya <pravinsuwasiya45@gmail.com>
2024-12-08 18:48:34 +0530

    new changes push

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// JavaScript for toggling the navigation menu on smaller screens
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navLinks = document.getElementById('nav-links');
 
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active'); // Toggle the 'active' class to show/hide the menu
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
 });
